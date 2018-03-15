572116facc3dbf6038a6bd09e09cc43131c61827
Kenn <kentot51810@gmail.com>
2018-03-15 19:26:32 +0800

    Initial commit

---
+++ src/RoundRobin.java
@@ -0,0 +1,233 @@
+import static java.lang.System.*;
+
+import java.text.*;
+import java.util.*;
+
+public class RoundRobin {
+
+    //Declaration of global variables
+    private static int numberOfProcess;
+    private static int quantum;
+    private static float turnaroundTime, waitingTime;
+    private static DecimalFormat decimalFormat;
+    private static List<Integer> listOfBurstTime;
+    private static List<Float> waitTimeList;
+    private static List<Float> turnaroundTimeList;
+    private static List<Map<String, Integer>> processBurstList;
+    private static List<Map<String, Float>> processTurnaroundList;
+    private static String[] processName;
+
+
+    private static void initializeInstanceVariables() {
+        listOfBurstTime = new ArrayList<>();
+        processBurstList = new ArrayList<>();
+        waitTimeList = new ArrayList<>();
+        decimalFormat = new DecimalFormat("0.#");
+        processTurnaroundList = new ArrayList<>();
+        turnaroundTimeList = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        initializeInstanceVariables();
+
+        out.println("\n\t\tROUND ROBIN SCHEDULING ALGORITHM");
+        out.print("\nEnter the total number of process: ");
+
+        numberOfProcess = getInput(numberOfProcess);
+
+        out.print("\n\nNumber of quantum:  ");
+        quantum = getInput(quantum);
+
+        out.println("\n\nEnter the burst time:\n");
+
+        setTheBurstTime();
+        saveProcessBurstList();
+
+        getTATList();
+        saveProcessTATList();
+
+        showGanttChart();
+
+        getAverageTurnAroundTime();
+        out.println("THE AVERAGE TURNAROUND TIME IS: " + decimalFormat.format(turnaroundTime));
+        getAverageWaitingTime();
+        out.println("THE AVERAGE WAITING TIME IS: " + decimalFormat.format(waitingTime));
+
+        out.print("\n\n");
+
+    }
+
+    private static void getAverageWaitingTime() {
+        waitingTime=0;
+        List<Float> arrayOfProcess = new ArrayList<>();
+        for (int index = 0; index < processName.length; index++){
+            ListIterator<Map<String, Float>> mapListIterator = processTurnaroundList.listIterator();
+            while (mapListIterator.hasNext()){
+                Map.Entry<String, Float> entry = mapListIterator.next().entrySet().iterator().next();
+                String key = entry.getKey();
+                if (key.equals(processName[index])){
+                    //TODO: Understand the context that I was trying to come up here a couple days ago.
+                    if (mapListIterator.hasPrevious()){
+                        Map<String, Float> previous = mapListIterator.previous();
+                        Set<Map.Entry<String, Float>> entries = previous.entrySet();
+                        Iterator<Map.Entry<String, Float>> iterator = entries.iterator();
+                        Map.Entry<String, Float> next = iterator.next();
+
+                        arrayOfProcess.add(next.getValue());
+                        mapListIterator.next();
+                    }
+                    else
+                        continue;
+
+                }
+                if (!mapListIterator.hasNext()){
+                    for (float value : arrayOfProcess) {
+                        out.print(decimalFormat.format(value));
+                    }
+                }
+            }
+        }
+        waitingTime /= numberOfProcess;
+    }
+
+    private static void showGanttChart() {
+        out.println("--------------------------------Gantt Chart--------------------------------");
+
+        out.print("PROCESS  ");
+        for (int index = 0; index < processBurstList.size(); index++) {
+            Map.Entry<String, Integer> entries = processBurstList.get(index).entrySet().iterator().next();
+            out.format("|%2s%5s%5s","",entries.getKey(),"");
+        }
+        out.println("|");
+
+        out.print("BURST    ");
+        for (int index = 0; index < processBurstList.size(); index++) {
+            Map.Entry<String, Integer> entries = processBurstList.get(index).entrySet().iterator().next();
+            out.format("|%2s%5s%5s","",entries.getValue(),"");
+        }
+        out.println("|");
+
+
+        out.format("%10s",0);
+        for (int index = 0; index < processBurstList.size(); index++) {
+            out.format("%13s",decimalFormat.format(turnaroundTimeList.get(index)));
+        }
+
+//        for (int index = 0; index < processBurstList.size(); index++){
+//            Map.Entry<String, Integer> entries = processBurstList.get(index).entrySet().iterator().next();
+//            out.print("PROCESS  ");
+//            out.format("|%2s%5s%5s","",entries.getKey(),"");
+//        }
+
+        out.println("\n\n");
+    }
+
+    private static void saveProcessBurstList() {
+
+        processName = new String[numberOfProcess];
+
+        for (int index = 0; index < numberOfProcess; index++) {
+            processName[index] = "P" + (index + 1);
+        }
+
+        int index = 0;
+        while (listOfBurstTime.get(index) != 0) {
+            Map<String, Integer> cpuRR = new HashMap<>();
+
+            int rr = 0;
+            if (listOfBurstTime.get(index) > quantum) {
+                rr = listOfBurstTime.get(index) - quantum;
+                cpuRR.put(processName[index], quantum);
+            } else {
+                //if the value of burst time is less than the quantum then subtract the value of burst time to itself.
+                rr = 0;
+                cpuRR.put(processName[index], listOfBurstTime.get(index));
+            }
+            if (listOfBurstTime.get(index) == 0) {
+                index++;
+                if (index == numberOfProcess) {
+                    index = 0;
+                }
+                continue;
+            }
+            processBurstList.add(cpuRR);
+
+            listOfBurstTime.set(index, rr);
+
+
+            index++;
+            if (index == numberOfProcess)
+                index = 0;
+        }
+    }
+
+    private static void saveProcessTATList() {
+        for (int index = 0; index < processBurstList.size(); index++){
+            Map.Entry<String, Integer> s = processBurstList.get(index).entrySet().iterator().next();
+            String processName = s.getKey();
+            float tat = turnaroundTimeList.get(index);
+            Map<String, Float> processTATMap = new HashMap<>();
+            processTATMap.put(processName, tat);
+
+            processTurnaroundList.add(processTATMap);
+        }
+    }
+
+    private static void setTheBurstTime() {
+        for (int index = 0; index < numberOfProcess; index++) {
+            out.print("P[" + (index + 1) + "]: ");
+            int burstTime = 0;
+            burstTime = getInput(burstTime);
+            listOfBurstTime.add(burstTime);
+        }
+
+    }
+
+    private static int getInput(int input) {
+        Scanner scn = new Scanner(in);
+        boolean continueLoop = true;
+        while (continueLoop) {
+            try {
+                input = scn.nextInt();
+                continueLoop = false;
+            } catch (InputMismatchException e) {
+                scn.nextLine();
+                err.println("Error! Please enter integers only");
+            }
+        }
+        return input;
+    }
+
+    private static void getTATList() {
+        for (int index = 0; index < processBurstList.size(); index++) {
+            Map.Entry<String, Integer> s = processBurstList.get(index).entrySet().iterator().next();
+            turnaroundTime += s.getValue();
+            turnaroundTimeList.add(turnaroundTime);
+        }
+
+        // return theWaitingTime;
+    }
+
+    private static void getAverageTurnAroundTime() {
+        turnaroundTime=0;
+        for (int index = 0; index < processName.length; index++){
+            ListIterator<Map<String, Float>> mapListIterator = processTurnaroundList.listIterator(processBurstList.size());
+            while (mapListIterator.hasPrevious()){
+                Map.Entry<String, Float> entry = mapListIterator.previous().entrySet().iterator().next();
+                String key = entry.getKey();
+                if (key.equals(processName[index])){
+//                    Map<String, Float> previous = mapListIterator.previous();
+//                    Set<Map.Entry<String, Float>> entries = previous.entrySet();
+//                    Iterator<Map.Entry<String, Float>> iterator = entries.iterator();
+//                    Map.Entry<String, Float> next = iterator.next();
+
+
+//                    Map.Entry<String, Float> processTAT = mapListIterator.previous().entrySet().iterator().next();
+                    turnaroundTime += entry.getValue();
+                    break;
+                }
+            }
+        }
+        turnaroundTime /= numberOfProcess;
+    }
+}
